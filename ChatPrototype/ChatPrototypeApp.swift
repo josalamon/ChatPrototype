b1afed60b4b27e652ef834552fe9a275e10f0c73
Josephine Salamon <jsalamon@lcs.on.ca>
2024-12-03 09:33:16 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Josephine Salamon on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Josephine Salamon on 2024-12-03.
+//
+
+import Testing
+@testable import ChatPrototype
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
